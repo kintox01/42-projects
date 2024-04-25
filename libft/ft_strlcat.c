6d79b07a9efd1b75a13176d8dc3ada441c3dfe59
Dav Guerin <davguerin@Ordinateur-portable-de-Dav.local>
2024-04-25 10:25:49 +0200

    push libft + start printf

---
+++ libft/ft_strlcat.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dguerin <dguerin@student.42nice.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/19 00:49:50 by dguerin           #+#    #+#             */
+/*   Updated: 2024/04/24 21:57:56 by dguerin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *d, const char *s, size_t dstsize)
+{
+	size_t	dst_len;
+	size_t	index;
+	size_t	i;
+
+	dst_len = ft_strlen(d);
+	index = 0;
+	while (d[index])
+		index++;
+	i = 0;
+	while (s[i] && (i + index + 1) < (dstsize))
+	{
+		d[index + i] = s[i];
+		i++;
+	}
+	if (i < dstsize)
+		d[index + i] = '\0';
+	if (dstsize <= dst_len)
+		return (ft_strlen(s) + dstsize);
+	else
+		return (ft_strlen(s) + dst_len);
+}
